ec3115e7e3bd7d404e156167e6f130a784770a67
charlycop <charlycop@free.fr>
2023-12-07 21:58:01 +0100

    Changed to Private Address, next step signature instead of private address

--- server/index.js
+++ server/index.js
@@ -7,10 +7,21 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "0294f61211e0491bbb3938e26bd795b7a349e50d27bdd14fa635713640f9d42859": 100,
+  "02fff69059dbcef9f66ffcb7660c28441d79033915d2d636a5e28e2e54a16ec81b": 50,
+  "0314d7b9d2a40830ee154e4db88d6e16b574d2c8c975a93e69c2f5dc2b4f43b26d": 75,
 };
+
+
+//private key: 91b0a49ef3140d586bb63fea134ced446a78c2771f7dd86a14c485884b3840c4
+//public key: 0294f61211e0491bbb3938e26bd795b7a349e50d27bdd14fa635713640f9d42859
+
+//private key: e34846bfd8291148f489c3f250955038bc9251b6172967277a903ad74b44ab2d
+//public key: 02fff69059dbcef9f66ffcb7660c28441d79033915d2d636a5e28e2e54a16ec81b
+
+//private key: c9edb7fcd536a60a3f85e8059174bb9dc148178dc3f083d937b567217dca261a
+//public key: 0314d7b9d2a40830ee154e4db88d6e16b574d2c8c975a93e69c2f5dc2b4f43b26d
+
 
 app.get("/balance/:address", (req, res) => {
   const { address } = req.params;
